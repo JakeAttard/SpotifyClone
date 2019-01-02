65dfaa0b9dbaf9d8ecbae444351e0cffc145119c
Jake Attard <jakeattard18@gmail.com>
2019-01-02 12:05:43 +1000

    Cleaning up Form

--- Register.php
+++ Register.php
@@ -1,13 +1,47 @@
 <?php
 
+function sanitizeFormUsername($inputText) {
+
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    $inputText = ucfirst(strtolower($inputText));
+    return $inputText;
+}
+
+function sanitizeFormPassword($inputText) {
+
+    $inputText = strip_tags($inputText);
+    return $inputText;
+}
+
 if(isSet($_POST['loginBtn'])) {
     //Login Button was pressed
     //echo "Login Button was pressed";
 }
 
 if(isSet($_POST['registerBtn'])) {
-    //Register Button was pressed
-    //echo "Register Button was pressed";
+
+    $registerUsername = sanitizeFormUsername($_POST['registerUsername']);
+
+    $registerFirstName = sanitizeFormString($_POST['registerFirstName']);
+
+    $registerLastName = sanitizeFormString($_POST['registerLastName']);
+
+    $registerEmail = sanitizeFormString($_POST['registerEmail']);
+
+    $registerConfirmEmail = sanitizeFormString($_POST['registerConfirmEmail']);
+
+    $registerPassword = sanitizeFormPassword($_POST['registerPassword']);
+
+    $registerConfirmPassword = sanitizeFormPassword($_POST['registerConfirmPassword']);
+
 }
 
 ?>
