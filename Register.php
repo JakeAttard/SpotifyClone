9791652f3fb1ed1325bf1754362783a7299166a3
Jake Attard <jakeattard18@gmail.com>
2019-01-06 11:19:37 +1000

    JQuery Form Added

--- Register.php
+++ Register.php
@@ -21,6 +21,9 @@
     <meta charset="UTF-8">
     <title>SpotifyClone</title>
     <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="assets/js/register.js"></script>
 </head>
 <body>
     <div id="background">
