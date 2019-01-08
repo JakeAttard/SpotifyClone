af27658dd65b0013bda46b6ae3cfa2237dfea747
Jake Attard <jakeattard18@gmail.com>
2019-01-08 11:18:44 +1000

    Now Playing Bar Created

--- index.php
+++ index.php
@@ -15,8 +15,12 @@ if(isset($_SESSION['userLoggedIn'])) {
 <head>
     <meta charset="UTF-8">
     <title>SpotifyClone</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+    <div id="nowPlayingBarContainer">
 
+    </div>
 </body>
 </html>
