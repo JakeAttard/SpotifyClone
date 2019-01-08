6de47be2c434317dd877dc92390b0e20d9e3a65d
Jake Attard <jakeattard18@gmail.com>
2019-01-08 23:25:52 +1000

    Play Buttons Added

--- index.php
+++ index.php
@@ -25,7 +25,28 @@ if(isset($_SESSION['userLoggedIn'])) {
 
             </div>
             <div id="nowPlayingCentre">
-
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="controlButton shuffle" title="Shuffle button">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
+                        <button class="controlButton previous" title="Previous button">
+                            <img src="assets/images/icons/previous.png" alt="Previous">
+                        </button>
+                        <button class="controlButton play" title="Play button">
+                            <img src="assets/images/icons/play.png" alt="Play">
+                        </button>
+                        <button class="controlButton pause" title="Pause button" style="display: none";>
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </button>
+                        <button class="controlButton next" title="Next button">
+                            <img src="assets/images/icons/next.png" alt="Next">
+                        </button>
+                        <button class="controlButton repeat" title="Repeat button">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </button>
+                    </div>
+                </div>
             </div>
             <div id="nowPlayingRight">
 
