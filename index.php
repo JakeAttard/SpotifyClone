b61c351f55fbb9b3df2a2c7fedb7affd904a97a9
Jake Attard <jakeattard18@gmail.com>
2019-01-09 22:06:04 +1000

    NavBar Items Created & Styled

--- index.php
+++ index.php
@@ -28,6 +28,24 @@ if(isset($_SESSION['userLoggedIn'])) {
                     <a href="index.php" class="logo">
                         <img src="assets/images/icons/logo.png">
                     </a>
+
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search</a>
+                        </div>
+                    </div>
+
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Jake Attard</a>
+                        </div>
+                    </div>
                 </nav>
             </div>
         </div>
