446c16476e1995257db207549b7d6ea3d89ba955
Jake Attard <jakeattard18@gmail.com>
2019-01-09 22:24:27 +1000

    NavBar Search Icon Added

--- index.php
+++ index.php
@@ -31,7 +31,9 @@ if(isset($_SESSION['userLoggedIn'])) {
 
                     <div class="group">
                         <div class="navItem">
-                            <a href="search.php" class="navItemLink">Search</a>
+                            <a href="search.php" class="navItemLink">Search
+                            <img src="assets/images/icons/search.png" class="icon" alt="Search">
+                            </a>
                         </div>
                     </div>
 
