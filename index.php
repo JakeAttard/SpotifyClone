eaf445c1c0f98ac6f99088e7ae152c0fcf503757
Jake Attard <jakeattard18@gmail.com>
2019-01-09 21:41:16 +1000

    Page Container Created

--- index.php
+++ index.php
@@ -19,69 +19,79 @@ if(isset($_SESSION['userLoggedIn'])) {
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-    <div id="nowPlayingBarContainer">
-        <div id="nowPlayingBar">
-            <div id="nowPlayingLeft">
-                <div class="content">
+
+    <div id="mainContainer">
+
+        <div id="topContainer">
+            
+        </div>
+
+        <div id="nowPlayingBarContainer">
+            <div id="nowPlayingBar">
+                <div id="nowPlayingLeft">
+                    <div class="content">
                     <span class="albumLink">
                         <img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtWork">
                     </span>
-                    <div class="trackInfo">
+                        <div class="trackInfo">
                         <span class="trackName">
                             <span>Happy Birthday</span>
                         </span>
-                        <span class="artistName">
+                            <span class="artistName">
                             <span>Jake Attard</span>
                         </span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div id="nowPlayingCentre">
-                <div class="content playerControls">
-                    <div class="buttons">
-                        <button class="controlButton shuffle" title="Shuffle button">
-                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                        </button>
-                        <button class="controlButton previous" title="Previous button">
-                            <img src="assets/images/icons/previous.png" alt="Previous">
-                        </button>
-                        <button class="controlButton play" title="Play button">
-                            <img src="assets/images/icons/play.png" alt="Play">
-                        </button>
-                        <button class="controlButton pause" title="Pause button" style="display: none";>
-                            <img src="assets/images/icons/pause.png" alt="Pause">
-                        </button>
-                        <button class="controlButton next" title="Next button">
-                            <img src="assets/images/icons/next.png" alt="Next">
-                        </button>
-                        <button class="controlButton repeat" title="Repeat button">
-                            <img src="assets/images/icons/repeat.png" alt="Repeat">
-                        </button>
+                <div id="nowPlayingCentre">
+                    <div class="content playerControls">
+                        <div class="buttons">
+                            <button class="controlButton shuffle" title="Shuffle button">
+                                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                            </button>
+                            <button class="controlButton previous" title="Previous button">
+                                <img src="assets/images/icons/previous.png" alt="Previous">
+                            </button>
+                            <button class="controlButton play" title="Play button">
+                                <img src="assets/images/icons/play.png" alt="Play">
+                            </button>
+                            <button class="controlButton pause" title="Pause button" style="display: none";>
+                                <img src="assets/images/icons/pause.png" alt="Pause">
+                            </button>
+                            <button class="controlButton next" title="Next button">
+                                <img src="assets/images/icons/next.png" alt="Next">
+                            </button>
+                            <button class="controlButton repeat" title="Repeat button">
+                                <img src="assets/images/icons/repeat.png" alt="Repeat">
+                            </button>
+                        </div>
+                        <div class="playBackBar">
+                            <span class="progressTime current">0.00</span>
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress"></div>
+                                </div>
+                            </div>
+                            <span class="progressTime remaining">0.00</span>
+                        </div>
                     </div>
-                    <div class="playBackBar">
-                        <span class="progressTime current">0.00</span>
+                </div>
+                <div id="nowPlayingRight">
+                    <div class="volumeBar">
+                        <button class="controlButton volume" title="Volume button">
+                            <img src="assets/images/icons/volume.png" alt="Volume">
+                        </button>
                         <div class="progressBar">
                             <div class="progressBarBg">
                                 <div class="progress"></div>
                             </div>
-                        </div>
-                        <span class="progressTime remaining">0.00</span>
-                    </div>
-                </div>
-            </div>
-            <div id="nowPlayingRight">
-                <div class="volumeBar">
-                    <button class="controlButton volume" title="Volume button">
-                        <img src="assets/images/icons/volume.png" alt="Volume">
-                    </button>
-                    <div class="progressBar">
-                        <div class="progressBarBg">
-                            <div class="progress"></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 </body>
 </html>
