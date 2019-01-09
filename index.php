0cbe55e2eb0e7d670dafe539405dcfc9d4327d2d
Jake Attard <jakeattard18@gmail.com>
2019-01-09 21:46:18 +1000

    NavBar Container Created

--- index.php
+++ index.php
@@ -23,7 +23,9 @@ if(isset($_SESSION['userLoggedIn'])) {
     <div id="mainContainer">
 
         <div id="topContainer">
-            
+            <div id="navBarContainer">
+
+            </div>
         </div>
 
         <div id="nowPlayingBarContainer">
