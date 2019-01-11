73aeefc30ea0fe6912ee54023fc4dbb94e420c03
Jake Attard <jakeattard18@gmail.com>
2019-01-12 00:09:23 +1000

    Artist of Album Displaying

--- album.php
+++ album.php
@@ -9,7 +9,13 @@ if(isset($_GET['id'])) {
 $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
 $album = mysqli_fetch_array($albumQuery);
 
-echo $album['title'];
+$artistId = $album['artist'];
+
+$artistQuery = mysqli_query($con, "SELECT * FROM  artists WHERE id='$artistId'");
+$artist = mysqli_fetch_array($artistQuery);
+
+echo $album['title'] . "<br>";
+echo $artist['name'];
 
 ?>
 
