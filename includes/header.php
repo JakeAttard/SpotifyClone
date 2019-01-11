8d0f4bae396d8f3457cd16d114152532a541caf8
Jake Attard <jakeattard18@gmail.com>
2019-01-11 19:58:44 +1000

    Header & Footer Files Created

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<?php
+include("includes/config.php");
+
+//session_destroy();
+
+if(isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+    header("Location: register.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SpotifyClone</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+<div id="mainContainer">
+
+    <div id="topContainer">
+
+        <?php include("includes/navBarContainer.php"); ?>
+
+        <div id="mainViewContainer">
+            <div id="mainContent">
--- index.php
+++ index.php
@@ -1,43 +1,5 @@
-<?php
-include("includes/config.php");
-
-//session_destroy();
-
-if(isset($_SESSION['userLoggedIn'])) {
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-} else {
-    header("Location: register.php");
-}
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>SpotifyClone</title>
-
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-<body>
-
-    <div id="mainContainer">
-
-        <div id="topContainer">
-
-        <?php include("includes/navBarContainer.php"); ?>
-
-        <div id="mainViewContainer">
-            <div id="mainContent">
-
-            </div>
-        </div>
-
-        </div>
-
-        <?php include("includes/nowPlayingBar.php"); ?>
-
-    </div>
+<?php include("includes/header.php"); ?>
 
 
-</body>
-</html>
+
+<?php include("includes/footer.php"); ?>
