d12d8387878f2b6d221cf84f452292fea95e97d3
Jake Attard <jakeattard18@gmail.com>
2019-01-13 00:28:08 +1000

    Songs Play Count Updating

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -38,8 +38,8 @@ $jsonArray = json_encode($resultArray);
                 $(".albumLink img").attr("src", album.artworkPath);
             });
 
-            audioElement.setTrack(track.path);
-            audioElement.play();
+            audioElement.setTrack(track);
+            playSong();
         });
 
         if(play) {
@@ -48,6 +48,11 @@ $jsonArray = json_encode($resultArray);
     }
 
     function playSong() {
+
+        if(audioElement.audio.currentTime == 0) {
+            $.post("includes/handlers/ajax/updatePlays.php", { songId: audioElement.currentlyPlaying.id });
+        }
+
         $(".controlButton.play").hide();
         $(".controlButton.pause").show();
         audioElement.play();
