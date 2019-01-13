a708621f74db3c53b8432d7d993488388a3dae47
Jake Attard <jakeattard18@gmail.com>
2019-01-13 23:52:26 +1000

    Repeat Button Added

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -92,8 +92,14 @@ $jsonArray = json_encode($resultArray);
         setTrack(trackToPlay, currentPlaylist, true);
     }
 
+    function setRepeat() {
+        repeat = !repeat;
+        var imageName = repeat ? "repeat-active.png" : "repeat.png";
+        $(".controlButton.repeat img").attr("src", "assets/images/icons/" + imageName);
+    }
+
     function setTrack(trackId, newPlaylist, play) {
-        
+
         currentIndex = currentPlaylist.indexOf(trackId);
         pauseSong();
 
@@ -180,7 +186,7 @@ $jsonArray = json_encode($resultArray);
                     <button class="controlButton next" title="Next button" onclick="nextSong()">
                         <img src="assets/images/icons/next.png" alt="Next">
                     </button>
-                    <button class="controlButton repeat" title="Repeat button">
+                    <button class="controlButton repeat" title="Repeat button" onclick="setRepeat()">
                         <img src="assets/images/icons/repeat.png" alt="Repeat">
                     </button>
                 </div>
