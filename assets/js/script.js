4a491229ff747b117e0df489b92f884e36d06104
Jake Attard <jakeattard18@gmail.com>
2019-01-13 19:51:14 +1000

    Drag Progress Bar on Click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var currentPlaylist = [];
 var audioElement;
+var mouseDown = false;
 
 function formatTime(seconds) {
     var time = Math.round(seconds);
@@ -46,5 +47,9 @@ function Audio() {
 
     this.pause = function() {
         this.audio.pause();
+    }
+
+    this.setTime = function(seconds) {
+        this.audio.currentTime = seconds;
     }
 }
