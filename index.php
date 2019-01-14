33044e16b3a116fc92ee65b9ca631f9312701ae5
Jake Attard <jakeattard18@gmail.com>
2019-01-15 00:49:14 +1000

    All Links replaced with Dynamic Links

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ include("includes/includedFiles.php");
 
             echo "<div class='gridViewItem'>
 
-                      <a href='album.php?id=" . $row['id'] . "'>
+                      <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
                           <img src='" . $row['artworkPath'] . "'>
                           
                           <div class='gridViewInfo'>"
                             . $row['title'] .
                           "</div>
-                      </a>
+                      </span>
                       
                   </div>";
         }
