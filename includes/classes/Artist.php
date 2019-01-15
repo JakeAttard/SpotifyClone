1c26942180c7208b4d36942d495516061a32c1f7
Jake Attard <jakeattard18@gmail.com>
2019-01-16 00:14:35 +1000

    Searching for Artists

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -10,6 +10,10 @@ class Artist {
         $this->id = $id;
     }
 
+    public function getId() {
+        return $this->id;
+    }
+
     public function getName() {
         $artistQuery = mysqli_query($this->con, "SELECT name FROM  artists WHERE id='$this->id'");
         $artist = mysqli_fetch_array($artistQuery);
--- search.php
+++ search.php
@@ -90,3 +90,30 @@ if(isset($_GET['term'])) {
 
     </ul>
 </div>
+
+<div class="artistsContainer borderBottom">
+    <h2>ARTISTS</h2>
+    <?php
+
+    $artistsQuery = mysqli_query($con, "SELECT id FROM artists WHERE name LIKE '$term%' LIMIT 10");
+
+    if(mysqli_num_rows($artistsQuery) == 0) {
+        echo "<span class='noResults'>No artists found matching " . $term . "</span>";
+    }
+
+    while($row = mysqli_fetch_array($artistsQuery)) {
+        $artistFound = new Artist($con, $row['id']);
+
+        echo "<div class='searchResultRow'>
+                <div class='artistName'>
+                    <span role='link' tabindex='0' onclick='openPage(\"artist.php?id=" . $artistFound->getId() ."\")'>
+                    "
+                    . $artistFound->getName() .
+                    "
+                    </span>
+                </div>
+              </div>";
+    }
+
+    ?>
+</div>
