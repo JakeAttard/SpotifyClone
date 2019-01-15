9d7be1ecb11637a89184648bdecf05dfcb89c2e4
Jake Attard <jakeattard18@gmail.com>
2019-01-15 11:54:42 +1000

    Now Playing Bar Links Work

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -162,12 +162,15 @@ $jsonArray = json_encode($resultArray);
                 var artist = JSON.parse(data);
 
                 $(".artistName span").text(artist.name);
+                $(".artistName span").attr("onclick", "openPage('artist.php?id=" + artist.id + "')");
             });
 
             $.post("includes/handlers/ajax/getAlbumJson.php", { albumId: track.album }, function(data) {
                 var album = JSON.parse(data);
 
                 $(".albumLink img").attr("src", album.artworkPath);
+                $(".albumLink img").attr("onclick", "openPage('album.php?id=" + album.id + "')");
+                $(".trackName span").attr("onclick", "openPage('album.php?id=" + album.id + "')");
             });
 
             audioElement.setTrack(track);
@@ -203,14 +206,14 @@ $jsonArray = json_encode($resultArray);
         <div id="nowPlayingLeft">
             <div class="content">
                     <span class="albumLink">
-                        <img src="" class="albumArtWork">
+                        <img role="link" tabindex="0" src="" class="albumArtWork">
                     </span>
                 <div class="trackInfo">
                         <span class="trackName">
-                            <span></span>
+                            <span role="link" tabindex="0"></span>
                         </span>
                     <span class="artistName">
-                            <span></span>
+                            <span role="link" tabindex="0"></span>
                         </span>
                 </div>
             </div>
