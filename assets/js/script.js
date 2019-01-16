a748e7bbdf2bdd9f7d914437f0e225fcf6fa289d
Jake Attard <jakeattard18@gmail.com>
2019-01-16 15:59:40 +1000

    createPlaylist Function Created

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,16 @@ function formatTime(seconds) {
     return minutes + ":" + extraZero + seconds;
 }
 
+function createPlaylist(username) {
+    var alert = prompt("Please enter the name of your playlist");
+
+    if(alert != null) {
+        $.post("includes/handlers/ajax/createPlaylist.php", { name: alert, username: username }).done(function() {
+            openPage("yourMusic.php");
+        });
+    }
+}
+
 function updateTimeProgressBar(audio) {
     $(".progressTime.current").text(formatTime(audio.currentTime));
     $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
--- yourMusic.php
+++ yourMusic.php
@@ -6,7 +6,7 @@ include("includes/includedFiles.php");
     <div class="gridViewContainer">
         <h2>PLAYLISTS</h2>
         <div class="buttonItems">
-            <button class="button green">NEW PLAYLIST</button>
+            <button class="button green" onclick="createPlaylist('jake')">NEW PLAYLIST</button>
         </div>
     </div>
 </div>
