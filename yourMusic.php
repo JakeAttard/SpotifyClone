23006ce3bc37e28e57881f90118d3bca2fdcd278
Jake Attard <jakeattard18@gmail.com>
2019-01-16 17:13:30 +1000

    Playlist Links Working

--- yourMusic.php
+++ yourMusic.php
@@ -22,7 +22,7 @@ include("includes/includedFiles.php");
 
                 $playlist = new Playlist($con, $row);
 
-                echo "<div class='gridViewItem'>
+                echo "<div class='gridViewItem' role='link' tabindex='0' onclick='openPage(\"playlists.php?id=" . $playlist->getId() ."\")'>
                         
                         <div class='playlistImage'>
                             <img src='assets/images/icons/playlist.png'>
